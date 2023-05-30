2e66ecc9612ebc41b2b39e5bb08bcbde04aa58d1
Maameadiefe <maameadiefe@gmail.com>
2023-05-30 16:35:02 -0700

    Changed the name of the file

---
+++ putchar.c
@@ -0,0 +1,11 @@
+#include <unistd>
+
+/**
+ * _putchur - accepts a char and returns it to the stdout
+ * @u: char to be returned
+ * Return: 0(Success)
+ */
+int _putchur(char u)
+{
+        return (write(1, &u, 1);
+}
